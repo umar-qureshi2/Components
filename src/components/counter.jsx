9b5e83053ac261b5b3ece8db2896329648f98cd6
Umar Qureshi <Umar.Qureshi@bentley.com>
2019-01-29 18:39:34 +0500

    code cleanup

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,17 +6,18 @@ class Counter extends Component {
   };
 
   render() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-
     return (
       <div>
-        <span style={{ fontSize: 22 }} className={classes}>
-          {this.getCount()}
-        </span>
+        <span className={this.getBadgeClasses()}>{this.getCount()}</span>
         <button className="btn btn-primary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = " badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   getCount() {
