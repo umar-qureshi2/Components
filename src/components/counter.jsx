2b00a5b3d37e5fdaedf5cb2ee60683eced1f95ae
Umar Qureshi <Umar.Qureshi@bentley.com>
2019-02-01 12:23:09 +0500

    noisy solution for binding event handlers

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,43 +2,43 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: [] // ["tag1", "tag2", "tag3"]
+    count: 0
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return "There are no tags!";
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+    console.log("Ctor", this);
+  }
 
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
+  handleIncrement() {
+    console.log("Increment clicked", this);
   }
 
   render() {
     return (
       <div>
-        {this.state.tags.length === 0 && <p>Please create a new tag</p>}
-        {/* <span className={this.getBadgeClasses()}>{this.getCount()}</span>
-        <button className="btn btn-primary btn-sm">Increment</button> */
-        this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.getCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-primary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
   }
 
-  //   getBadgeClasses() {
-  //     let classes = " badge m-2 badge-";
-  //     classes += this.state.count === 0 ? "warning" : "primary";
-  //     return classes;
-  //   }
+  getBadgeClasses() {
+    let classes = " badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 
-  //   getCount() {
-  //     const { count } = this.state;
-  //     return count === 0 ? "Zero" : count;
-  //   }
+  getCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
 
 export default Counter;
